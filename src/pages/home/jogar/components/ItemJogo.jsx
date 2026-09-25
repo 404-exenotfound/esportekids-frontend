ba6bb1d05a7623c4278e9a2aeb3c2efe36a538c1
Bilheiro <joaofeijon320@gmail.com>
2026-09-25 20:39:32 -0300

    feat: img capas

--- src/pages/home/jogar/components/ItemJogo.jsx
+++ src/pages/home/jogar/components/ItemJogo.jsx
@@ -1,11 +1,31 @@
+import atletismo from "../../../../assets/atletismo.jpeg";
+import basquete from "../../../../assets/basquete.jpeg";
+import boliche from "../../../../assets/boliche.jpeg";
+import futebol from "../../../../assets/futebol.jpeg";
+
+const imagensJogos = {
+  atletismo,
+  basquete,
+  boliche,
+  futebol,
+};
+
 export const ItemJogo = ({ jogo, setJogo, nomeJogo }) => {
+  const imagem = imagensJogos[nomeJogo];
+
   return (
     <div
       className={`personagem ${jogo === nomeJogo ? "personagem-selecionado" : ""}`}
       onClick={() => setJogo(nomeJogo)}
     >
       <div className="personagem-imagem">
-        {/* Imagem da Nirvana vai aqui */}
+        {imagem && (
+          <img
+            src={imagem}
+            alt={nomeJogo}
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        )}
       </div>
 
       <span>{nomeJogo}</span>
